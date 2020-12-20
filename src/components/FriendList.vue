7d5a40180a9e4d61006c79150c85d10d9d23821c
TheNightmareX <TheNightmareX@foxmail.com>
2020-12-20 16:56:18 +0800

    Update the friends list interface.
    Use a menu to operate instead.

--- src/components/FriendList.vue
+++ src/components/FriendList.vue
@@ -1,55 +1,61 @@
 <template>
-  <v-list class="py-0">
-    <v-subheader>好友</v-subheader>
+  <FriendOperationMenu>
+    <template v-slot="{ showMenu }">
+      <v-list class="py-0">
+        <v-subheader>好友</v-subheader>
 
-    <v-list-group
-      v-for="{ id, user, chatroom } of relations.accepted"
-      :key="id"
-      @change="$emit('change', chatroom)"
-    >
-      <template #activator>
-        <v-list-item-title>{{ user.username }}</v-list-item-title>
-      </template>
-      <v-list-item>
-        <v-list-item-title class="d-flex justify-space-around">
-          <v-btn icon @click="destroy(id)">
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </v-list-item-title>
-      </v-list-item>
-    </v-list-group>
+        <v-list-item-group>
+          <v-list-item
+            v-for="relation of relations.accepted"
+            :key="relation.id"
+            @click="$emit('change', relation.chatroom)"
+            @contextmenu.prevent="showMenu($event, relation)"
+          >
+            <v-list-item-title>{{ relation.user.username }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
 
-    <v-subheader v-if="relations.pending.length >= 1">待处理</v-subheader>
+        <v-subheader v-if="relations.pending.length >= 1">待处理</v-subheader>
 
-    <v-list-group v-for="{ id, user, asSender } of relations.pending" :key="id">
-      <template #activator>
-        <v-list-item-title>
-          {{ user.username }}
-        </v-list-item-title>
-      </template>
-      <v-list-item>
-        <v-list-item-title class="d-flex justify-space-around">
-          <v-btn icon @click="destroy(id)">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-btn v-if="!asSender" icon @click="accept(user.id)">
-            <v-icon>mdi-check</v-icon>
-          </v-btn>
-        </v-list-item-title>
-      </v-list-item>
-    </v-list-group>
+        <v-list-group
+          v-for="{ id, user, asSender } of relations.pending"
+          :key="id"
+        >
+          <template #activator>
+            <v-list-item-title>
+              {{ user.username }}
+            </v-list-item-title>
+          </template>
+          <v-list-item>
+            <v-list-item-title class="d-flex justify-space-around">
+              <v-btn icon @click="destroy(id)">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-btn v-if="!asSender" icon @click="accept(user.id)">
+                <v-icon>mdi-check</v-icon>
+              </v-btn>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-group>
 
-    <v-divider></v-divider>
+        <v-divider></v-divider>
 
-    <slot name="bottom"></slot>
-  </v-list>
+        <slot name="bottom"></slot>
+      </v-list>
+    </template>
+  </FriendOperationMenu>
 </template>
 
 <script>
 import * as apis from "../apis";
 import { mapGetters } from "vuex";
+import FriendOperationMenu from "./FriendOperationMenu";
 
 export default {
+  name: "FriendList",
+
+  components: { FriendOperationMenu },
+
   computed: {
     ...mapGetters("friendRelations", ["relations"]),
   },
@@ -61,13 +67,6 @@ export default {
      */
     accept(targetUserID) {
       apis.friendRelations.create(targetUserID);
-    },
-    /**
-     *
-     * @param {number} id
-     */
-    destroy(id) {
-      apis.friendRelations.destroy(id);
     },
   },
 };
--- src/components/Notifier.vue
+++ src/components/Notifier.vue
@@ -6,6 +6,8 @@
 
 <script>
 export default {
+  name: "Notifier",
+
   data: () => ({
     color: "info",
     text: "",
